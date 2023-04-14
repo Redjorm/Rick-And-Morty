982ec745232de209920deba7863eb47599049d22
ANGELGOMEZ2007 <aagc8969@gmail.com>
2023-04-14 16:30:28 -0400

    input de la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
+import { useState } from "react";
 import "./App.css";
 import LocationCard from "./components/LocationCard";
 import ResidentCard from "./components/ResidentCard";
 import useFetch from "./hooks/useFetch";
 import getRandomLocation from "./utils/getRandomLocation";
+import Select from 'react-select'
+import locationsAll from './Json/locationsAll.json'
 
 function App() {
-  const randomLocation = getRandomLocation();
+  const [inputValue, setinputValue] = useState(getRandomLocation())
+
+
+
   const location = useFetch(
-    `https://rickandmortyapi.com/api/location/${randomLocation}`
+    `https://rickandmortyapi.com/api/location/${inputValue}`
   );
 
-  console.log(randomLocation);
-  console.log(location);
+  const inputSelect = []
+
+  for (let i = 0; i < locationsAll.results.length; i++) {
+    inputSelect.push({"value": locationsAll.results[i].value, "label": locationsAll.results[i].value + ". " + locationsAll.results[i].label})
+  }
+  
+
+  const handlerLocation = e => {
+    setinputValue(e.value)
+  }
 
   return (
     <div className="container">
+
       <header className="header">
         <img className="img__header" src="/images/headerIMG.jpg" alt="" />
       </header>
       {/* INPUT ANGEL */}
+      <Select className='select__input' onChange={handlerLocation} options={inputSelect} />
       {/* NEEL PANTALLA DE CARGA Y MENSAJE DE ERROR */}
       <div className="info">
         <LocationCard location={location} />
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,14 +3,14 @@ import React, { useEffect, useState } from 'react'
 
 const useFetch = (url) => {
   const [state, setState] = useState()
-
+  
   useEffect(() => {
     axios.get(url)
     .then(res => setState(res.data))
     .catch(error => console.log(error))
-  }, [])
+  }, [url])
 
-  return state
+  return state 
 }
 
 export default useFetch
